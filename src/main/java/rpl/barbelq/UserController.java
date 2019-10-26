18440b4775f06954e844edd4e95bb9c406cfac88
Yohanes Samuel Ero Martono <54306688+yohanessamuel00@users.noreply.github.com>
2019-10-26 16:29:03 +0700

    UI
    UI

---
+++ src/main/java/rpl/barbelq/UserController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl.barbelq;
+
+/**
+ *
+ * @author yohan
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+public class UserController {
+    
+}
