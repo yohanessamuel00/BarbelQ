d05bcc71d3eacb9efc448e778eb096ffd47a85c1
Yohanes Samuel Ero Martono <54306688+yohanessamuel00@users.noreply.github.com>
2019-10-29 00:49:52 +0700

    Revert "Update FXML"
    This reverts commit 9ddf417cb5410ce43b417538c88b5035fc3dfdb8.

--- src/main/java/rpl/barbelq/LoginController.java
+++ src/main/java/rpl/barbelq/LoginController.java
@@ -14,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
